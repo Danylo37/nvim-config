d273c8df368fedd9ba5a6e8e25574da1f5451872
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2026-07-29 03:01:16 +0300

    feat: improve UI with Noice and Notify
    - add Noice for enhanced command line and LSP UI
    - add nvim-notify for animated notifications
    - configure notification appearance
    - configure Noice presets
    - update plugin lockfile

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -57,4 +57,52 @@ return {
 			},
 		},
 	},
+
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			local notify = require("notify")
+
+			notify.setup({
+				stages = "fade",
+				timeout = 3000,
+				render = "default",
+				top_down = true,
+				background_colour = "#000000",
+			})
+
+			vim.notify = notify
+		end,
+	},
+
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+		config = function()
+			require("noice").setup({
+				lsp = {
+					progress = {
+						enabled = true,
+					},
+					hover = {
+						enabled = true,
+					},
+					signature = {
+						enabled = true,
+					},
+				},
+
+				presets = {
+					bottom_search = false,
+					command_palette = true,
+					long_message_to_split = true,
+					lsp_doc_border = true,
+				},
+			})
+		end,
+	},
 }
